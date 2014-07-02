b53ca49bce5db32176d9fa956190d7ada33b3d27
oyvble (Øyvind Bleka) <oyvble@hotmail.com>
2014-07-02 12:19:22 +0000

    updated genDataset
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesdnamix@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gammadnamix/R/genDataset.R
+++ pkg/gammadnamix/R/genDataset.R
@@ -7,19 +7,18 @@
 #' @param popFreq A list of allele frequencies for a given population.
 #' @param mu Expected peak heights for an allele of a contributor.
 #' @param sd Standard deviation of peak heights for an allele of a contributor.
-#' @param PrD Fixed Allele drop-out probability for each contributors. 
 #' @param sorted Boolean for wheter sorting the contributors with respect to decreasingly mixture proportions.
+#' @param thresh Threshold given
 #' @return List with elements Mx,mixData,refData
 
-genDataset = function(nC,popFreq,mu=10000,sd=1000,PrD = 0,sorted=FALSE) {
+genDataset = function(nC,popFreq,mu=1000,sd=1000,sorted=FALSE,thresh=50) {
   require(gtools)
   nL<-length(popFreq)
   Mx=rdirichlet(1,rep(1,nC))  #true Mx for contributors
   if(sorted)  Mx  <- sort(Mx,decreasing=TRUE)
   refData <- list() 
   mixData <- list(adata=list(),hdata=list()) 
-  if(length(PrD)==1) PrD <- rep(PrD,nC)
-  if(length(PrD)!=nC) stop("Wrong size of PrD given")
+
   for(i in 1:nL) {
    refData[[i]] <- list()
    mixA = numeric()
@@ -28,7 +27,7 @@ genDataset = function(nC,popFreq,mu=10000,sd=1000,PrD = 0,sorted=FALSE) {
     Asim <- refData[[i]][[s]] <-  sample(names(popFreq[[i]]),size=2,prob=popFreq[[i]],replace=TRUE)
     Hsim <- qnorm(runif(2,0.5,1))*sd+(mu*Mx[s])
 #    Hsim <-  abs(rnorm(2,mu*Mx[s],sd))
-    droped <- sample(0:1,2,replace=TRUE,prob=c(1-PrD[s],PrD[s]))
+    droped <- Hsim<thresh #sample(0:1,2,replace=TRUE,prob=c(1-PrD[s],PrD[s]))
     mixA = c(mixA, Asim[droped==0]) #keep not droped
     mixH = c(mixH, Hsim[droped==0]) #keep not droped
    }
