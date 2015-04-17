34d738f21ca46c03c34add5bcae64fa0528e2f9c
oyvble (Øyvind Bleka) <oyvble@hotmail.com>
2015-04-17 09:29:42 +0000

    gammadnamix renamed to euroformix
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bayesdnamix@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/euroformix/R/contLikINT.R
@@ -0,0 +1,76 @@
+
+#' @title contLikINT
+#' @author Oyvind Bleka <Oyvind.Bleka.at.fhi.no>
+#' @description contLikINT marginalizes the likelihood of the STR DNA mixture given some assumed a bayesian model by numerical integration.
+#' @details The procedure are doing numerical integration to approximate the marginal probability over the noisance parameters. Mixture proportions have flat prior.
+#' 
+#' Function calls procedure in c++ by using the package Armadillo and Boost.
+#'
+#' @param nC Number of contributors in model.
+#' @param samples A List with samples which for each samples has locus-list elements with list elements adata and hdata. 'adata' is a qualitative (allele) data vector and 'hdata' is a quantitative (peak heights) data vector.
+#' @param popFreq A list of allele frequencies for a given population.
+#' @param lower Lower bounds of parameters. Must be in following order: mx1,..,mx_(nC-1),mu,sigma,beta,xi.
+#' @param upper Upper bounds of parameters. Must be in following order: mx1,..,mx_(nC-1),mu,sigma,beta,xi.
+#' @param refData Reference objects has locus-list element [[i]] with a list element 'r' which contains a 2 long vector with alleles for each references.
+#' @param condOrder Specify conditioning references from refData (must be consistent order). For instance condOrder=(0,2,1,0) means that we restrict the model such that Ref2 and Ref3 are respectively conditioned as 2. contributor and 1. contributor in the model. condOrder=-1 means the reference is known-non contributor!
+#' @param knownRef Specify known references from refData (index). For instance knownRef=(1,2) means that reference 1 and 2 is known allele samples in the hypothesis. This is affected by fst-correction.
+#' @param xi A numeric giving stutter-ratio if it is known. Default is NULL, meaning it is integrated out.
+#' @param prC A numeric for allele drop-in probability. Default is 0.
+#' @param reltol Required relative tolerance error of evaluations in integration routine. Default is 0.001.
+#' @param threshT The detection threshold given. Used when considering probability of allele drop-outs.
+#' @param fst is the coancestry coeffecient. Default is 0.
+#' @param lambda Parameter in modeled peak height shifted exponential model. Default is 0.
+#' @param pXi Prior function for xi-parameter (stutter). Flat prior on [0,1] is default.
+#' @param kit shortname of kit: {"ESX17","ESI17","ESI17Fast","ESX17Fast","Y23","Identifiler","NGM","ESSPlex","ESSplexSE","NGMSElect","SGMPlus","ESX16", "Fusion","GlobalFiler"}
+#' @return ret A list(margL,deviation,nEvals) where margL is Marginalized likelihood for hypothesis (model) given observed evidence, deviation is the confidence-interval of margL, nEvals is number of evaluations.
+#' @export 
+#' @references Hahn,T. (2005). CUBA - a library for multidimensional numerical integration. Computer Physics Communications, 168(2),78-95.
+#' @keywords continuous, Bayesian models, Marginalized Likelihood estimation
+
+
+contLikINT = function(nC,samples,popFreq,lower,upper,refData=NULL,condOrder=NULL,knownRef=NULL,xi=NULL,prC=0,reltol=0.01,threshT=50,fst=0,lambda=0,pXi=function(x)1,kit=NULL){
+ require(cubature) 
+ if(length(lower)!=length(upper)) stop("Length of integral limits differs")
+ np2 <- np <- nC + 2 + sum(is.null(xi)) #number of unknown parameters
+ if(length(lower)!=np) stop("Length of integral limits differs from number of unknown parameters specified")
+ ret <- prepareC(nC,samples,popFreq,refData,condOrder,knownRef,kit)
+ nodeg  <- is.null(kit) #boolean whether modeling degradation FALSE=YES, TRUE=NO
+ if(nodeg) {
+   np2 <- np2 - 1
+   if(length(lower)>np2) {
+    lower <- lower[-(nC+2)] #remove beta boundary
+    upper <- upper[-(nC+2)] #remove beta boundary
+   }
+ }
+ if(length(lower)!=np2) stop("The length integral limits was not the same as number of parameters!")
+ liktheta <- function(theta) {   
+  theta2 <- theta[1:(nC+1)] #take out mx,mu,sigma
+  if(nodeg) {
+    theta2 <- c(theta2,1) #add beta=1 to parameters
+  } else {
+    theta2 <- c(theta2,theta[nC+2]) #add beta to parameters
+  }
+  if(is.null(xi)) {  #if xi unknown
+    theta2 <- c(theta2,theta[np2]) #add xi param to parameters
+  } else { #if xi known
+    theta2 <- c(theta2,xi) #add xi param to parameters
+  }
+  Cval  <- .C("loglikgammaC",as.numeric(0),as.numeric(theta2),as.integer(np),ret$nC,ret$nK,ret$nL,ret$nS,ret$nA,ret$obsY,ret$obsA,ret$CnA,ret$allAbpind,ret$nAall,ret$CnAall,ret$Gvec,ret$nG,ret$CnG,ret$CnG2,ret$pG,ret$pA, as.numeric(prC), ret$condRef,as.numeric(threshT),as.numeric(fst),ret$mkvec,ret$nkval,as.numeric(lambda),ret$bp,as.integer(0),PACKAGE="gammadnamix")[[1]]
+  if(is.null(xi)) Cval <- Cval + log(pXi(theta2[np2])) #weight with prior
+  return(exp(Cval)) #weight with prior of tau and stutter.
+ }
+ nU <- nC-ret$nK #number of unknowns
+ bisectMx <- (nC==2 && nU==2) #if exact 2 unknown contributors in the hypothesis
+ if(bisectMx) lower[1] <- 0.5 #restrict outcome of mixture proportions
+ foo <- adaptIntegrate(liktheta, lowerLimit = lower , upperLimit = upper , tol = reltol)
+ val <- foo$integral
+ dev <- val + c(-1,1)*foo$error
+ nEvals <- foo[[3]]
+ if(nU>1) { #if more than 1 unknown 
+  comb <- factorial(nU)
+  val <- comb*val
+  dev <- comb*dev
+ }
+ return(list(margL=val,deviation=dev,nEvals=nEvals))
+}
+
---
+++ pkg/euroformix/R/deconvolve.R
@@ -0,0 +1,116 @@
+#' @title deconvolve
+#' @author Oyvind Bleka <Oyvind.Bleka.at.fhi.no>
+#' @description deconvolve ranks the set of the most conditional posterior probability of genotypes the STR DNA mixture given a fitted model under a hypothesis.
+#' @details The procedure calculates the likelihood for each single locus. Then it combines the most probable genotypes from each loci to produce a ranked list of deconvolved profiles.
+#' 
+#' Function calls procedure in c++ by using the package Armadillo and Boost.
+#'
+#' @param mlefit Fitted object using contLikMLE function.
+#' @param alpha Required sum of the listed posterior probabilities.
+#' @param maxlist The ranked deconvolved profile list will not exceed this number (used to avoid endless search).
+#' @param unknownonly A boolean whether table should only contain the unknown genotypes or both known and unknown genotypes.
+#' @return ret A list(table1,rankG,pG) where rankG is the ranked genotypes with corresponding probabilities in pG. table1 is a formated version of these two.
+#' @export
+#' @references Cowell,R.G. et.al. (2014). Analysis of forensic DNA mixtures with artefacts. Applied Statistics, 64(1),1-32.
+#' @keywords deconvolution
+deconvolve = function(mlefit,alpha=0.95,maxlist=1000,unknownonly=TRUE){
+ theta2 <- theta <- mlefit$fit$thetahat #condition on mle parameter
+ model <- mlefit$model #take out assumed model with given data
+ locs <- names(model$popFreq)
+ nL <- length(locs)
+ nC <- model$nC
+ xi <- model$xi
+ np <- length(theta)#number of unknown parameters
+ loglikYtheta <- function() {   #call c++- function: length(theta)=nC+1
+   Cval  <- .C("loglikgammaC",as.numeric(0),as.numeric(theta),as.integer(np),ret$nC,ret$nK,ret$nL,ret$nS,ret$nA,ret$obsY,ret$obsA,ret$CnA,ret$allAbpind,ret$nAall,ret$CnAall,ret$Gvec,ret$nG,ret$CnG,ret$CnG2,ret$pG,ret$pA, as.numeric(model$prC), ret$condRef,as.numeric(model$threshT),as.numeric(model$fst),ret$mkvec,ret$nkval,as.numeric(model$lambda),ret$bp,as.integer(0),PACKAGE="gammadnamix")[[1]]
+   return(Cval) #weight with prior of tau and 
+ }
+ nodeg <- is.null(model$kit) #check for degradation
+ if(nodeg) theta <- c(theta[1:(nC+1)],1) #insert beta variable equal 1
+ if(!is.null(xi)) {
+  theta <- c(theta,as.numeric(xi))
+ } else {
+  if(nodeg) theta <- c(theta,theta2[np])
+ }
+
+ #Using information in ret to try out different genotypes:
+
+ #Step 1) Calculate L(E|g,thetahat) for each marker
+ dlist <- list()
+ GClist <- list()
+ for(loc in locs) {
+  samples <- lapply(model$samples,function(x) x[loc])
+  ret <- prepareC(nC=nC,samples,popFreq=model$popFreq[loc],refData=model$refData[loc],condOrder=model$condOrder,knownRef=model$knownRef,kit=model$kit)
+  uind <- which(ret$condRef==-1) #unknown genotype indices
+  nU <- length(uind) #number of unknowns
+  if(nU==0) stop("There was no unknown genotype profiles to estimate. The evaluation will not be done!")
+  ret$nK <- nC  #number of known is equal number of contributors
+  Gset <- matrix(ret$Gvec,ncol=2) #genotype possibilities
+  Glist <- list()
+  for(k in 1:nU) {
+   Glist[[k]] <- 1:nrow(Gset)
+  }
+  combGind <- expand.grid(Glist) #get all combinations
+  combGind <- as.matrix(combGind,nrow=nrow(combGind))
+  dvec <- rep(NA,nrow(combGind))
+  #calculate for each genotypes:
+  for(gind in 1:nrow(combGind)) { #for each possible genotypes:
+   ret$condRef[uind] <- as.integer(combGind[gind,] - 1) #genotypes to consider
+   dvec[gind] <- loglikYtheta() 
+   dvec[gind] <- dvec[gind] + sum(log(ret$pG[combGind[gind,]])) #add genotype probability as well
+  }
+  isOK <- !is.infinite(dvec)
+  combGind <- combGind[isOK,]
+  dvec <- dvec[isOK]
+  rank <- order(dvec,decreasing=TRUE)
+  dlist[[loc]] <- dvec[rank] 
+
+  if(is.null(dim(combGind))) { #threat the case of one unknown
+   GClist[[loc]] <- as.matrix(combGind[rank]) 
+  } else {
+   GClist[[loc]] <- combGind[rank,]
+  }
+ }
+
+ #Step 2) Combine markers to create full profiles (this is it's own function):
+ loghdval <- mlefit$fit$loglik 
+ if(is.null(xi)) loghdval <- loghdval - log(model$pXi(theta2[np])) #reported likval has taken pXi into account
+ rankGlist <- combineRank(dlist,loghdval=loghdval,alpha=alpha,maxsearch=maxlist)
+ pG <- rankGlist$pG
+ Gset  <- rankGlist$rankG
+
+ kvec <- 1:nC
+ if(unknownonly) kvec <- uind  
+
+ #Step 3) Convert rank-list to list with allele-names
+ Glist <- getGlist(model$popFreq) #get genotype list with genotypes and corresponding frequencies
+ deconvlist <- list()
+ for(i in 1:nL) { #convert names for each locus
+  rankgeno <- GClist[[locs[i]]][Gset[,i],]
+  if(is.null(dim(rankgeno))) rankgeno <- as.matrix(rankgeno) #make matrix again
+  rankgenos <- numeric()
+  for(k in 1:nC) { #for each contributor
+   if(k%in%uind) { #if unknown contributor
+    geno <- Glist[[locs[i]]]$G[rankgeno[,which(k==uind)],] #get allele named genotype
+   } else if(!unknownonly) { #if known contributors in addition(they are given in reference) 
+    geno <- sort(model$refData[[locs[i]]][[which(model$condOrder==k)]])
+    geno <- matrix( rep(geno,nrow(rankgeno)),ncol=2,byrow=TRUE)
+   } else {
+    next #skip to next contributor
+   }
+   geno <- paste0(geno[,1],"/",geno[,2])
+   rankgenos <- cbind(rankgenos,geno)
+  }
+  colnames(rankgenos) <- paste0("g",kvec)
+  deconvlist[[locs[i]]] <- rankgenos 
+ }
+
+ #Step4) Create table layouts:
+ table1 <- numeric()
+ for(loc in locs) { #convert names for each locus
+  table1 <- cbind(table1,deconvlist[[loc]])
+ }
+ table1 <-  cbind(table1,pG)
+ colnames(table1) <- c(paste0(c(t(replicate(length(kvec),locs))),"_g",kvec),"posterior")
+ return(list(table1=table1,rankG=deconvlist,pG=pG))
+} #end function
